c2be919054e07e71a026d4f29b03ad99cd1aef25
Mujeeb <mujib517@gmail.com>
2019-01-29 15:10:33 +0530

    Rohith/Mujeeb Story1: Tab design

--- src/movies/MovieGrid.jsx
+++ src/movies/MovieGrid.jsx
@@ -7,6 +7,11 @@ import './MovieGrid.css';
 
 class MovieGrid extends Component {
 
+  constructor() {
+    super();
+    this.state = { selectedTab: 0 };
+  }
+
   componentDidMount() {
     this.props.fetchMovies()
   }
@@ -19,15 +24,37 @@ class MovieGrid extends Component {
     return this.props.movies.error || false ? this.showError() : this.showMovies();
   }
 
+  applyClass(tabIndex) {
+    return this.state.selectedTab === tabIndex ? 'active-tab tab-btn' : 'tab-btn';
+  }
+
+  onTabClick(tabIndex) {
+    this.setState({ selectedTab: tabIndex });
+  }
+
   showMovies() {
     return (
-      <div className="movies-container">
-        {this.props.movies.items.map((movie) =>
+      <div >
+        <div>
+          <button className={this.applyClass(0)} onClick={() => this.onTabClick(0)}>NOW RUNNING</button>
+          <button className={this.applyClass(1)} onClick={() => this.onTabClick(1)}>COMING SOON</button>
+        </div>
 
-            <MovieItem key={movie.name} movie={movie}/>
-        )}
+        <div className="movies-container">
+          {this.state.selectedTab === 0 ? this.showRunningMovies() : this.showUpcomingMovies()}
+        </div>
       </div>
     );
+  }
+
+  showRunningMovies() {
+    return this.props.movies.items.map((movie) =>
+      <MovieItem key={movie.name} movie={movie} />
+    )
+  }
+
+  showUpcomingMovies(){
+    return null;
   }
 
   showProgress() {
--- src/movies/MovieItem.jsx
+++ src/movies/MovieItem.jsx
@@ -6,16 +6,16 @@ import { Card } from '@material-ui/core';
 const MovieItem = ({ movie }) => {
   const imageUrl = `https://img.spicinemas.in/resources/images/movies/${movie.slug}/150x207.jpg`;
   return (
-    <div class="movie-wrapper">
-    <Card className="movie">
-      <img className="movie-image" alt={movie.name} src={imageUrl} />
-      <div className="movie-details">
-        <h4>{movie.name}</h4>
-        <h6>{movie.experiences}</h6>
-      </div>
-     </Card>
-     </div>
-    
+    <div className="movie-wrapper">
+      <Card className="movie">
+        <img className="movie-image" alt={movie.name} src={imageUrl} />
+        <div className="movie-details">
+          <h4>{movie.name}</h4>
+          <h6>{movie.experiences}</h6>
+        </div>
+      </Card>
+    </div>
+
   )
 }
 
