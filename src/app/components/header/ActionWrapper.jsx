1757ee44b7752d9c2742a3d9a8996b2dcd5df13b
avinash.saraf <avinash.saraf@thoughtworks.com>
2019-01-29 15:54:51 +0530

    Social media share link

--- src/app/components/header/ActionWrapper.jsx
+++ src/app/components/header/ActionWrapper.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import '../../assets/stylesheets/header.css'
 import {
-  FacebookIcon,
-  TwitterIcon
+  FacebookIcon, FacebookShareButton,
+  TwitterIcon, TwitterShareButton
 } from 'react-share';
 
 const ActionWrapper = () => {
   return (
     <div className="action-wrapper">
-      <FacebookIcon url="www" className="action-item social-icon" />
-      <TwitterIcon url="www" className="action-item social-icon" />
+      <FacebookShareButton url="https://www.facebook.com/ThoughtWorks/" >
+        <FacebookIcon className="action-item social-icon" />
+      </FacebookShareButton>
+      <TwitterShareButton url="https://twitter.com/thoughtworks">
+        <TwitterIcon url="www" className="action-item social-icon" />
+      </TwitterShareButton>
       <Button variant="outlined" className='action-item jcinemas-login-button'>
         Login/SignUp
     </Button>
