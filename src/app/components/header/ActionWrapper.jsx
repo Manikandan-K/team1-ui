38af369629a57fbd40c089c90c7d871d162e2a81
Rohit <in-rohit.mulange@Rohits-MacBook-Pro.local>
2019-01-30 10:21:55 +0530

    Rohit/Mujeeb | Fix Font Awesome include css

--- src/app/components/header/ActionWrapper.jsx
+++ src/app/components/header/ActionWrapper.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../../assets/stylesheets/header.css'
-import '../../../../node_modules/font-awesome/css/font-awesome.css'
+import 'font-awesome/css/font-awesome.css'
 
 const ActionWrapper = () => {
   return (
     <div className="action-wrapper">
-      <a className="button-icon--blue" url="https://www.facebook.com/ThoughtWorks/" >
+      <a className="button-icon--blue" href="https://www.facebook.com/ThoughtWorks/" >
         <i class="fa fa-facebook"></i>
       </a>
-      <a className="button-icon--skyblue" url="https://twitter.com/thoughtworks">
+      <a className="button-icon--skyblue" href="https://twitter.com/thoughtworks">
         <i className="fa fa-twitter"></i>
       </a>
       <button className='action-item jcinemas-login-button'>
