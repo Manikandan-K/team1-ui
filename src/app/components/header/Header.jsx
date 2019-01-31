9b0f08e1fc3e19a7b0ed61b813fde3975b705f6d
Rohit <in-rohit.mulange@Rohits-MacBook-Pro.local>
2019-01-31 11:11:07 +0530

    Rohit [ Story-2 ] Improve Interaction experience

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -4,10 +4,12 @@ import ActionWrapper from './ActionWrapper'
 import '../../App.css';
 import './Header.css';
 
+
 const Header = () => (
   <div className="page-header">
     <TitleWrapper/>
     <ActionWrapper/>
+
   </div >
 );
 
